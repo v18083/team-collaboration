e6e9436780d2f76bcaa613518bf0c30edfb7009f
edwardmalupenga <edwardmalupenga97@gmail.com>
2025-09-29 21:16:13 +0200

    have worked on declaration of variables and also getting user input

---
+++ rectangle_area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Declare variables
+    float length, width, area;
+
+    // Get user input
+    printf("Enter length of rectangle: ");
+    scanf("%f", &length);
+    printf("Enter width of rectangle: ");
+    scanf("%f", &width);
